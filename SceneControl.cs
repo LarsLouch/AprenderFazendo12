92980fd3eeac97484d7b56df1f8f14b269a08e15
João Lázaro Brito Dias <63989543+LarsLouch@users.noreply.github.com>
2022-01-31 05:19:25 -0300

    Add files via upload

---
+++ SceneControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public void Replay()
+    {
+        Invoke("DelayReplay", 0.7f);
+    }
+
+    private void DelayReplay()
+    {
+        // SceneManager.GetActiveScene().name 取得目前啟動場景的名稱
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BackToMenu()
+    {
+        Invoke("DelayBackToMenu", 0.7f);
+    }
+
+    private void DelayBackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Invoke("DelayQuit", 0.7f);
+    }
+
+    private void DelayQuit()
+    {
+        Application.Quit();
+    }
+}
